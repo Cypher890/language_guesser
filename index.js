f5226e378331583873abf84d971aa0148aee020f
Cypher890 <abdulsuhaib07@gmail.com>
2022-07-16 14:27:18 +0530

    adde colors and input to the program

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-import{ createRequire } from "module"             // these two lines are used
+import { createRequire } from "module"             // these two lines are used
 const require = createRequire(import.meta.url)    // 
 
-import {franc} from "franc";
+import { franc } from "franc";
+const colors = require("colors")
 const langs = require('langs')
-const langCode = franc('Alle menslike wesens word vry')
+const input = process.argv[2]
+const langCode = franc(input)
+
 // console.log(franc('Alle menslike wesens word vry'));
 
-const language = langs.where("3", langCode)
-console.log(language.name)
+if (langCode === 'und') {
+    console.log("sorry, couldn't figure it out! try with another sample ".red)
+} else {
+    const language = langs.where("3", langCode)
+    console.log(`the language is ${language.name}`.green)
+}
